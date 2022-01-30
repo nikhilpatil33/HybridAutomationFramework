6fd1b7fcd911515391d299c6f77b3abd2af862fa
nikhil <adventurepistons@gmail.com>
2022-01-31 00:14:34 +0530

    89

--- myerth.com/src/test/java/pagefactory/bookspage.java
+++ myerth.com/src/test/java/pagefactory/bookspage.java
@@ -1,5 +1,24 @@
 package pagefactory;
 
-public class bookspage {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
+public class bookspage {
+	 WebDriver driver;
+		@FindBy(id="userName") WebElement fullname;
+		@FindBy(id="userEmail") WebElement userEmail;
+		@FindBy(id="currentAddress") WebElement currentAddress;
+		@FindBy(id="permanentAddress") WebElement permanentAddress;
+		@FindBy(id="submit") WebElement submit;
+		
+		@FindBy(xpath="//*[@id=\"app\"]/div/div/div[2]/div[1]/div/div/div[1]/span/div/div[1]") WebElement elements;
+		@FindBy(xpath="//*[@id=\"item-0\"]/span") WebElement textbox;
+		public bookspage(WebDriver driver)
+		{
+			this.driver=driver;
+			PageFactory.initElements(driver, this);
+		}
+		
 }
